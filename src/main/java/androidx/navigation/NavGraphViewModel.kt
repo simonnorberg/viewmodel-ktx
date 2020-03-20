2d9ab7b46e12d3c0b4aa35e8d7966ad67556aa22
Simon Norberg <simon.norberg@gmail.com>
2020-03-20 20:43:12 +0100

    Add navgraph savedstate factory

--- src/main/java/androidx/navigation/NavGraphViewModel.kt
+++ src/main/java/androidx/navigation/NavGraphViewModel.kt
@@ -3,6 +3,8 @@ package androidx.navigation
 import androidx.annotation.IdRes
 import androidx.annotation.MainThread
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.AbstractSavedStateViewModelFactory
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelLazy
 import androidx.lifecycle.ViewModelProvider
@@ -25,6 +27,32 @@ inline fun <reified VM : ViewModel> Fragment.navGraphViewModelFactory(
             @Suppress("UNCHECKED_CAST")
             override fun <T : ViewModel> create(modelClass: Class<T>): T {
                 return viewModelProducer() as T
+            }
+        }
+    }
+    return ViewModelLazy(VM::class, storeProducer, factoryProducer)
+}
+
+@MainThread
+inline fun <reified VM : ViewModel> Fragment.navGraphSavedStateViewModelFactory(
+    @IdRes navGraphId: Int,
+    noinline viewModelProducer: (SavedStateHandle) -> VM
+): Lazy<VM> {
+    val backStackEntry: NavBackStackEntry by lazy {
+        findNavController().getBackStackEntry(navGraphId)
+    }
+    val storeProducer: () -> ViewModelStore = {
+        backStackEntry.viewModelStore
+    }
+    val factoryProducer: () -> ViewModelProvider.Factory = {
+        object : AbstractSavedStateViewModelFactory(this, null) {
+            @Suppress("UNCHECKED_CAST")
+            override fun <T : ViewModel> create(
+                key: String,
+                modelClass: Class<T>,
+                handle: SavedStateHandle
+            ): T {
+                return viewModelProducer(handle) as T
             }
         }
     }
